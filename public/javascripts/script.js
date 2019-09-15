b73bd28511de2e0f60b673571c473da1ea6ed423
Andrei Fedotov <anddfedotov@gmail.com>
2019-09-15 00:12:33 -0700

    Added edit pages and linkedin link

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,18 +1,27 @@
 const main = function() {
-  $(".menu a").on('click', function(event) {
-    var scrollTarget = $(this).attr('to');
+  const host = 'http://' + window.location.host;
+
+  $('.menu a').on('click', function(event) {
+    const scrollTarget = $(this).attr('to');
     $('html, body').animate({
       scrollTop: $(scrollTarget).offset().top
     }, 2000);
     return false;
   });
 
-  $(".admin a").on('click', function(event) {
-  	var code = $(".admin input").val();
-  	$.post($(location).attr('href'), code, function() {
-      window.location.href = "http://127.0.0.1:3000/admin/edit";
+  $('.admin a.login').on('click', function(event) {
+  	const code = $('.admin input').val();
+  	$.post(host + '/login', code, function(data) {
+      window.location.href = host + '/admin/edit';
     });
   	return false;
+  });
+
+  $('.edit a.logout').on('click', function(event) {
+    $.post(host + '/logout', function(data) {
+      window.location.href = host + '/admin';
+    });
+    return false;
   });
 }
 
